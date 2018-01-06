e5e0e03439339761bf27e6838e1e1b63a9c8fe4c
robinz62 <robinz672@gmail.com>
2018-01-05 22:03:49 -0500

    can now add move

--- src/AddCommandPanel.java
+++ src/AddCommandPanel.java
@@ -32,6 +32,10 @@ public class AddCommandPanel extends JPanel {
 	private static final String WAITPANEL = "Wait";
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * Creates the panel containing all the controls to add a command.
+	 * @param list the list to add the command to
+	 */
 	public AddCommandPanel(DefaultListModel<Command> list) {
 		this.list = list;
 		this.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
@@ -41,20 +45,23 @@ public class AddCommandPanel extends JPanel {
 		String[] cbItems = { CLICKPANEL, MOVEPANEL, TYPEPANEL, WAITPANEL };
 		JComboBox<String> cb = new JComboBox<>(cbItems);
 		
-		JPanel moveCard = new JPanel();
-		
-		JPanel typeCard = new JPanel();
-		
-		JPanel waitCard = new JPanel();
-		
 		JPanel cards = new JPanel(new CardLayout());
 		cards.setBorder(BorderFactory.createTitledBorder("Parameters"));
 		cards.add(createClickCard(), CLICKPANEL);
+		cards.add(createMoveCard(), MOVEPANEL);
+//		cards.add(createTypeCard(), TYPEPANEL);
+//		cards.add(createWaitCard(), WAITPANEL);
 		
 		this.add(cb, BorderLayout.PAGE_START);
 		this.add(cards, BorderLayout.CENTER);
 		
-		
+		cb.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String chosenPanel = (String) cb.getSelectedItem();
+				((CardLayout) cards.getLayout()).show(cards, chosenPanel);
+			}
+		});
 		
 		((CardLayout) cards.getLayout()).show(cards, CLICKPANEL);
 	}
@@ -119,7 +126,8 @@ public class AddCommandPanel extends JPanel {
 		clickLoc.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				// TODO implement this
+				// TODO
+				// https://stackoverflow.com/questions/2419555/how-to-obtain-mouse-click-coordinates-outside-my-window-in-java
 			}
 		});
 		
@@ -221,6 +229,93 @@ public class AddCommandPanel extends JPanel {
 		clickPanel.add(add, gc);
 		
 		return clickPanel;
+	}
+	
+	/**
+	 * Creates the move card containing the move parameters.
+	 * @return the move card
+	 */
+	private JPanel createMoveCard() {
+		GridBagConstraints gc;
+		JPanel movePanel = new JPanel(new GridBagLayout());
+		movePanel.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
+		
+		// Field for x-coordinate
+		JPanel xCoord = new JPanel(new GridBagLayout());
+		gc = new GridBagConstraints();
+		gc.gridx = 0;
+		gc.gridy = 0;
+		xCoord.add(new JLabel("X: "), gc);
+		gc = new GridBagConstraints();
+		gc.gridx = 1;
+		gc.gridy = 0;
+		JTextField xTextField = new JTextField();
+		xTextField.setPreferredSize(new Dimension(80, 23));
+		xTextField.setFont(new Font("Consolas", Font.PLAIN, 12));
+		xCoord.add(xTextField, gc);
+		
+		// Field for y-coordinate
+		JPanel yCoord = new JPanel(new GridBagLayout());
+		gc = new GridBagConstraints();
+		gc.gridx = 0;
+		gc.gridy = 0;
+		yCoord.add(new JLabel("Y: "), gc);
+		gc = new GridBagConstraints();
+		gc.gridx = 1;
+		gc.gridy = 0;
+		JTextField yTextField = new JTextField();
+		yTextField.setPreferredSize(new Dimension(80, 23));
+		yTextField.setFont(new Font("Consolas", Font.PLAIN, 12));
+		yCoord.add(yTextField, gc);
+		
+		// Button to click location
+		JButton clickLoc = new JButton("Click Location");
+		clickLoc.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO same as in clickPanel
+				// https://stackoverflow.com/questions/2419555/how-to-obtain-mouse-click-coordinates-outside-my-window-in-java
+			}
+		});
+		
+		// Button to add command
+		JButton add = new JButton("Add");
+		add.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					int x = Integer.parseInt(xTextField.getText());
+					int y = Integer.parseInt(yTextField.getText());
+					list.addElement(new Move(x, y));
+					((Window) (AddCommandPanel.this.getTopLevelAncestor())).dispose();
+				} catch (NumberFormatException e) {
+					return;
+				}
+			}
+		});
+		
+		// Add and style components
+		gc = new GridBagConstraints();
+		gc.gridx = 0;
+		gc.gridy = 0;
+		gc.insets = new Insets(8, 0, 0, 0);
+		movePanel.add(xCoord, gc);
+		gc = new GridBagConstraints();
+		gc.gridx = 0;
+		gc.gridy = 1;
+		gc.insets = new Insets(8, 0, 0, 0);
+		movePanel.add(yCoord, gc);
+		gc = new GridBagConstraints();
+		gc.gridx = 0;
+		gc.gridy = 2;
+		gc.insets = new Insets(8, 0, 0, 0);
+		movePanel.add(clickLoc, gc);
+		gc = new GridBagConstraints();
+		gc.gridx = 0;
+		gc.gridy = 3;
+		gc.insets = new Insets(8, 0, 0, 0);
+		movePanel.add(add, gc);
+		
+		return movePanel;
 	}
 
 }
